1a386e6ec1815ed96b3b68187964b23995b3ad0d
Javier Domingo <javierdo1@gmail.com>
2010-04-29 01:14:23 +0200

    Puesto bonito el texto

--- profesor/Teclado.java
+++ profesor/Teclado.java
@@ -7,34 +7,48 @@ package profesor;
 
 
 import java.io.*;
-public class Teclado {
-public static char LeeCaracter() {
-char ch;
-try {
-ch = LeeCadena().charAt(0);
-} catch( Exception e ) {
-ch = 0;
-}
-return ch;
-}
-public static String LeeCadena() {
-BufferedReader br =
-new BufferedReader(new InputStreamReader(System.in));
-String str;
-try {
-str = br.readLine();
-} catch( Exception e ) {
-str = "";
-}
-return str;
-}
-public static int LeeEntero() {
-int num;
-try {
-num = Integer.parseInt( LeeCadena().trim() );
-} catch( Exception e ) {
-num = 0;
-}
-return num;
-}
+public class Teclado
+{
+    public static char LeeCaracter()
+    {
+        char ch;
+        try
+        {
+            ch = LeeCadena().charAt(0);
+        }
+        catch( Exception e )
+        {
+            ch = 0;
+        }
+        return ch;
+    }
+
+    public static String LeeCadena()
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        try
+        {
+            str= br.readLine();
+        }
+        catch( Exception e )
+        {
+            str = "";
+        }
+        return str;
+    }
+    
+    public static int LeeEntero()
+    {
+        int num;
+        try
+        {
+            num = Integer.parseInt( LeeCadena().trim() );
+        }
+        catch( Exception e )
+        {
+            num = 0;
+        }
+        return num;
+    }
 }
