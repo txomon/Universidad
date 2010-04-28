2ecbcbc5ed28041553cbee7cfde4c9e4fc35eeb3
Javier Domingo <javierdo1@gmail.com>
2010-04-28 23:55:50 +0200

    Creado Jugable.java

---
+++ Jugable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author Javier
+ */
+public interface Jugable {
+    public abstract void Juega();
+    public void MuestraNombre();
+    public void MuestraInfo();
+
+
+}
