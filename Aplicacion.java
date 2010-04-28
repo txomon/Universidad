686b2607770570edc2e93a3dd730075fde08d1f9
Javier Domingo <javierdo1@gmail.com>
2010-04-29 00:07:29 +0200

    Modificada Aplicacion

--- Aplicacion.java
+++ Aplicacion.java
@@ -8,28 +8,59 @@
  *
  * @author Javier
  */
-public class Aplicacion {
-    public static void main(String argv[])
+public class Aplicacion 
+{
+    public static Jugable EligeJuego()
     {
 
+        int opcion;
+        Jugable juegos[]=new Jugable[3];
 
-        JuegoAdivinaNumero jugadornumerico=new JuegoAdivinaNumero(3,7);
-        JuegoAdivinaPar jugadorpar=new JuegoAdivinaPar(3,4);
-        JuegoAdivinaImpar jugadorimpar=new JuegoAdivinaImpar(3,9);
+        juegos[0]=new JuegoAdivinaNumero(3,7);
+        juegos[1]=new JuegoAdivinaPar(3,4);
+        juegos[2]=new JuegoAdivinaImpar(3,9);
 
-        jugadornumerico.MuestraNombre();
-        jugadornumerico.MuestraInfo();
-        jugadornumerico.Juega();
+        System.out.print("A que juego deseas jugar?\n\n" +
+                "\tAl juego adivina número (0)\n" +
+                "\tAl juego adivina un par (1)\n" +
+                "\tAl juego adivina impar  (2)\n\n\t" +
+                "Elije:");
+        do
+            opcion=Teclado.LeeEntero();
+        while(opcion<0||opcion>2);
 
-        jugadorpar.MuestraNombre();
-        jugadorpar.MuestraInfo();
-        jugadorpar.Juega();
-
-        jugadorimpar.MuestraNombre();
-        jugadorimpar.MuestraInfo();
-        jugadorimpar.Juega();
-
-        return;
+        return juegos[opcion];
     }
+    public static void main(String argv[])
+    {
+        Jugable opcion;
+        boolean seguir;
+        do
+        {
+            opcion=EligeJuego();
+
+            opcion.MuestraNombre();
+            opcion.MuestraInfo();
+            opcion.Juega();
+
+            seguir=Preguntar();
+        }
+        while(seguir);
+
+    }
+
+    public static boolean Preguntar()
+    {
+        char marcado;
+
+        System.out.println("\n\nCacho friky quieres probar a perder otra vez?([S]/N)");
+
+        marcado=Teclado.LeeCaracter();
+        if(marcado!='n'&&marcado!='N')
+            return true;
+        return false;
+
+    }
+
 
 }
