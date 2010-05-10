6f899ea66d8c24b6c99308534114ab565d7c8af6
unknown <alumno@.(none)>
2010-05-10 16:03:31 +0200

    corregido el 1 para que muestre las 2 por separado

--- Practica4/Practica4.m
+++ Practica4/Practica4.m
@@ -5,17 +5,57 @@ if s~=6
         case 1
             t=-10:0.1:10;
             uno=rectpuls(t);
-            dos=tripuls(t-2);
-            x=uno+dos
+            dos=rectpuls(t-2);
+            x=uno+dos;
             
+            hold on;
+            subplot(2,1,1);
+            plot(t,uno);
+            plot(t,dos,'g');
+            plot(t,x,'r');
+            hold off;
+            
+            
+            subplot(2,1,2);
+            uno=tripuls(t);
+            dos=tripuls(t-2);
+            x=uno+dos;
             hold on;
             plot(t,uno);
             plot(t,dos,'g');
             plot(t,x,'r');
             
+            hold off;
+            
             %Se superpone la suma, pero si te fijas se ve el verde y el
             %azul por debajo
         case 2
-            t=
+            t=-10:0.1:10;
+            
+            
+            subplot(2,2,1);
+            uno=tripuls(t);
+            dos=cos(t);
+            x=dos*uno;
+            plot(t,uno);
+            plot(t,dos,'g');
+            plot(t,x,'r');
+            
+            subplot(2,2,2);
+            uno=rectpuls(t);
+            x=dos*uno;
+            plot(t,uno);
+            plot(t,dos,'g');
+            plot(t,x,'r');
+            
+            %supongo que el 3 ese por ahi suelto significa multiplicar...
+            
+            subplot(2,2,3);
+            uno=tripuls(t)*3;
+            dos=4*t/t;
+            plot(t,uno);
+            plot(t,dos,'g');
+            plot(t,x,'r');
+            
     end
 end
