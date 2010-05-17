096e2d9778824596b8593d905c1311f282262136
Javier Domingo <txomon1@gmail.com>
2010-05-17 15:48:51 +0200

    ahora va bien

--- Practica5/Practica5.m
+++ Practica5/Practica5.m
@@ -8,14 +8,14 @@ if s~=5
             u5=[zeros(1,95) ones(1,106)];
             U5=[zeros(1,106) ones(1,95)];
             u=[zeros(1,100) ones(1,101)];
-            u10=[zeros(1,90) ones(1,111)];
+            U10=[zeros(1,111) ones(1,90)];
             
             x=u5-U5;
-            h=exp(-3*n)*(u-u10);
+            h=exp(-3*n).*(u-U10);
             
             y=conv(x,h);
             
-            plot(N,y);
+            stem(N,y);
             
     end
 end
