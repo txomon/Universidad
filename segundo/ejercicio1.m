4f7dcfcb0055d3dd38b0dbbd23c2f0d8e6670727
javier <javierdo1@gmail.com>
2010-10-01 12:15:39 +0200

    primera parte hecha

--- segundo/ejercicio1.m
+++ segundo/ejercicio1.m
@@ -1,8 +1,10 @@
-function [ output_args ] = ejercicio1( input_args )
+function [ Precib ] = ejercicio1( Ptrans, Lcanal, Aten, Ganan)
 %EJERCICIO1 Summary of this function goes here
 %  Detailed explanation goes here
+% como Aten=10*log(Ptrans/Precib) entonces la funcion, aislando seria
 
-
+DbW=log10(Ptrans);
+Precib=DbW-Lcanal*Aten+Ganan;
 
 
 end
