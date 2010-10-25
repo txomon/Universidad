a391c15e428bb5d51e66883062bf4b8d03f6b5ff
javier <javierdo1@gmail.com>
2010-10-25 08:54:52 +0200

    Hecho el apartado a y b

--- 2/TC/Practica 3/ejercicio2.m
+++ 2/TC/Practica 3/ejercicio2.m
@@ -2,6 +2,9 @@ function [ output_args ] = ejercicio2( audiowav )
 %EJERCICIO2 Summary of this function goes here
 %  Detailed explanation goes here
 
+
+
+%% a)
 hold on;
 subplot(3,1,1);
 hist(audiowav.data,1000);
@@ -14,23 +17,26 @@ hist(xinvalaw,1000);
 %sound(xinvalaw);
 pause;
 hold off;
-hist(xinvalaw,1000);
+
+%% b)
 
 subplot(3,1,1);
-hold on;
-sound(audiowav.data);
-pause;
 x1=q_unif(audiowav.data, 16,-1,1);
 hist(x1,1000);
-sound(x1);
-pause;
-x2=alaw(audiowav.data,87.6);
+hold on;
 subplot(3,1,2);
-sound(x2);
-pause;
+x2=alaw(audiowav.data,87.6);
 hist(x2,1000);
 subplot(3,1,3);
 hist(audiowav.data,1000);
+hold off;
 
+sound(x1);
+pause;
+sound(x2);
+pause;
+sound(audiowav.data);
+
+subplot(1,1,1);
 xerr=audiowav.data-x2;
 hist(xerr,1000);
