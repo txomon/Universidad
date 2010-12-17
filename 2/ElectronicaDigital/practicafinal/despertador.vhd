14ce31c73393413fc26e98911443959758df0eaa
unknown <Alumno@.xa.bi.ehu.es>
2010-12-17 12:04:34 +0100

    seguimos avanzando en la cc.12

--- 2/ElectronicaDigital/practicafinal/despertador.vhd
+++ 2/ElectronicaDigital/practicafinal/despertador.vhd
@@ -42,7 +42,7 @@ end despertador;
 architecture Behavioral of despertador is
 ---------------------------------------------------------------
 ------------ Contadores ---------------------------------------
-signal lento, camb_lento			:	integer;
+signal lento, camb_lento, cambio_display		:	integer;
 
 ---------------------------------------------------------------
 ------------ Variables de las Entradas-------------------------
@@ -56,14 +56,14 @@ signal hora, minuto, segundo		: integer;
 
 ---------------------------------------------------------------
 ------------ Señales intermedias ------------------------------
-signal lento, alarmita, enable_m, enable_h, camb_lent : std_logic;
+signal alarmita, enable_m, enable_h, camb_lent			: std_logic;
 signal enable_minuto, enable_hora, alarma					: std_logic;
 signal digito														: std_logic_vector (7 downto 0);
 
 ---------------------------------------------------------------
 ------------ Salidas ------------------------------------------
-signal mux_camb_hora, mux_camb_min, despertador_programado					: std_logic;
-signal mux_camb_desp_min,mux_camb_desp_hra , mux_display, enable_lento : std_logic;
+signal mux_camb_hra, mux_camb_min, despertador_programado					: std_logic;
+signal mux_camb_desp_min,mux_camb_desp_hora , mux_display, enable_lento : std_logic;
 
 ---------------------------------------------------------------
 ------------ Estados-------------------------------------------
@@ -71,6 +71,11 @@ signal mux_camb_desp_min,mux_camb_desp_hra , mux_display, enable_lento : std_log
 type estados is (inicio, reset, est_hora, est_desp, cmb_min, cmb_hra, cmb_desp_hra, cmb_desp_min);
 signal estado_actual, estado_siguiente : estados;
 ---------------------------------------------------------------
+------------------- señales para el display -------------------
+type 4digitos is array integer range 0 to 3;
+signal digito 		: 4digitos;
+signal digito_ssg : integer;
+
 begin
 ---------------------------------------------------------------
 ------------------------ Entradas------------------------------
@@ -78,7 +83,7 @@ cmb_hora		<=	swt(0);
 cmb_desp		<=	swt(1);
 rst 			<=	btn(0);
 conf_hora	<= btn(3);
-conf_minuto	<= btn(2);
+conf_min 	<= btn(2);
 
 
 ---------------------------------------------------------------
@@ -87,6 +92,32 @@ process( mclk )
 begin
 	if rising_edge(mclk) then
 		estado_actual <= estado_siguiente;
+		
+----------------------------------		
+			if cambio_display = 0 then
+				cambio_display<=cambio_display+1;
+				an<="0111";
+				digito_ssg=digito(0);
+			elsif cambio_display = 250000 then
+				cambio_display<=cambio_display+1;
+				an<="1011";
+				digito_ssg=digito(1);
+			elsif cambio_display = 500000 then
+				cambio_display<=cambio_display+1;
+				an<="1101";
+				digito_ssg=digito(2);
+			elsif cambio_display = 750000 then
+				cambio_display<=cambio_display+1;
+				an<="1110";
+				digito_ssg=digito(3);
+			elsif cambio_display = 999999 then
+				cambio_display <= 0;
+				digito_ssg=digito(1);
+			else
+				cambio_display <= cambio_display+1;
+			end if;
+----------------------------------			
+			
 	end if;
 end process;
 
@@ -96,8 +127,11 @@ end process;
 process ( rst,conf_min,conf_hora,cmb_hora,cmb_desp,estado_actual )
 begin
 	case estado_actual is
-		when reset =>
-			if rst=1 then
+		
+		when reset =>			
+			enable_lento <= '1';
+			despertador_programado <='0';
+			if rst='1' then
 				estado_siguiente <= reset;
 			else
 				estado_siguiente <= inicio;
@@ -105,17 +139,101 @@ begin
 			
 		when inicio =>
 			if cmb_hora='1' and cmb_desp='0' and rst='0' then
+				enable_lento <= '0';
 				estado_siguiente <= est_hora;
 			elsif cmb_hora='0' and cmb_desp='1' and rst='0' then
 				estado_siguiente <= est_desp;
 			elsif rst='1' then
 				estado_siguiente <= reset;
 			else
-				estado_siguiente <= inicio
+				estado_siguiente <= inicio;
 			end if;
-		
-	
-
+		---------------------------------------------	
+			when est_hora =>
+				mux_camb_hra <='0';
+				mux_camb_min <='0';
+				if conf_hora='1' and conf_min='0' and rst='0' and cmb_hora='1' then
+					estado_siguiente <= cmb_hra;
+				elsif conf_hora='0' and conf_min='1' and rst='0' and cmb_hora='1' then
+					estado_siguiente <= cmb_min;
+				elsif cmb_hora='0' and rst='0' then
+					enable_lento <= '1';
+					estado_siguiente <= inicio;
+				elsif rst='1' then
+					estado_siguiente <= reset;
+				else
+					estado_siguiente <= est_hora;
+				end if;
+				---------------------------------------------
+				when cmb_hra =>
+					mux_camb_hra <='1';
+					if conf_hora <='1' and cmb_hora='1' and rst='0' then
+						estado_siguiente <= cmb_hra;
+					elsif cmb_hora='0' and rst='0' then
+						enable_lento <= '1';
+						estado_siguiente <= inicio;
+					elsif rst='1' then
+						estado_siguiente<=reset;
+					else
+						estado_siguiente <= est_hora;
+					end if;
+				
+				when cmb_min =>
+					mux_camb_min <='1';
+					if conf_min <='1' and cmb_hora='1' and rst='0' then
+						estado_siguiente <= cmb_min;
+					elsif cmb_hora='0' and rst='0' then
+						enable_lento <= '1';
+						estado_siguiente <= inicio;
+					elsif rst='1' then
+						estado_siguiente<=reset;
+					else
+						estado_siguiente <= est_hora;
+					end if;
+				---------------------------------------------
+			when est_desp =>
+				mux_display <='1';
+				mux_camb_desp_hora <='0';
+				mux_camb_desp_min <='0';
+				if conf_hora='1' and conf_min='0' and rst='0' and cmb_desp='1' then
+					estado_siguiente <= cmb_desp_hra;
+				elsif conf_hora='0' and conf_min='1' and rst='0' and cmb_desp='1' then
+					estado_siguiente <= cmb_desp_min;
+				elsif cmb_desp='0' and rst='0' then
+					estado_siguiente <= inicio;
+				elsif rst='1' then
+					estado_siguiente <= reset;
+				else
+					estado_siguiente <= est_desp;
+				end if;
+				---------------------------------------------
+				when cmb_desp_hra =>
+					mux_camb_desp_hora <='1';
+					despertador_programado <='1';
+					if conf_min='1' and cmb_hora='1' and rst='0' then
+						estado_siguiente <= cmb_desp_min;
+					elsif cmb_hora='0' and rst='0' then
+						estado_siguiente <= inicio;
+					elsif rst='1' then
+						estado_siguiente <= reset;
+					else
+						estado_siguiente <= est_desp;
+					end if;
+					
+				when cmb_desp_min =>
+					mux_camb_desp_min <='1';
+					despertador_programado <='1';
+					if conf_min='1' and cmb_desp='1' and rst='0' then
+						estado_siguiente <= cmb_desp_min;
+					elsif cmb_desp='0' and rst='0' then
+						estado_siguiente <= inicio;
+					elsif rst='1' then
+						estado_siguiente<=reset;
+					else
+						estado_siguiente <= est_desp;
+					end if;
+				---------------------------------------------
+			end case;
 end process;
 
 end Behavioral;
