8790d662b9102672cbcdc1d6ba5da2889061ae75
Javier Domingo <javierdo1@gmail.com>
2011-03-15 01:55:11 +0100

    Anadida porfin la practica final

---
+++ 2/labSED/final/Final.asm
@@ -0,0 +1,86 @@
+;
+; Comienzo con la proyección del ejercicio de practica final de Sistemas Digitales. El objetivo ahora, va a ser conseguir hacer un movil que mande y
+; reciba SMS. Una vez acabado eso, se pensará en la ampliación del proyecto a más. Para conseguirlo, se dispone de un modem GSM maestro100 que tiene
+; todo lo necesario para poder enviar y recibir mensajes.
+;
+; De momento me centraré en conseguir el menú. Según vaya progresando en la teoría y en el planteamiento del proyecto, iré ampliando esta documentación
+;
+;
+;
+;
+	LIST	P=16F887; Para el compilador
+	INCLUDE	"P16F887.INC"; Para las EQU del micro
+;*****************************************************************************************************************************************
+;***************************************** LAS EQUIVALENCIAS Y LAS VARIABLES *************************************************************
+;*****************************************************************************************************************************************
+;******** VARIABLES DE LA RSI *********;
+SAVEW	EQU	20
+SAVEST	EQU	21
+SAVEFSR	EQU	22
+SAVEPCL	EQU	23
+
+;******** EQUIVALENCIAS DE PUERTOS ****;
+PORTPAD	EQU	PORTB
+TRISPAD	EQU	TRISB 
+
+
+
+; Y aqui empezamos propiamente a programar
+	ORG	003
+	GOTO	PROG; Como siempre, le decimos que vaya al programa y que nos deje el espacio siguiente a nosotros a nosotros para la reti
+
+
+RSI:
+	ORG	004;Especifico el comienzo de la RSI al lugar de una interrupción
+	BCF	INTCON,GIE; Como voy a hacer un sistema de RSI muy completo, no
+	; necesito interrupciones
+;Espacio de Trabajo
+	MOVWF	SAVEW;Guardo el Espacio de Trabajo
+;Status
+	MOVF	STATUS,W;Pongo el status en el Espacio de trabajo
+	MOVWF	SAVEST;Guardo el status en su espacio correspondiente
+	CLRF	STATUS;Me coloco en el espacio de trabajo 0
+;FSR
+	MOVF	FSR,W;Muevo el valor del registro de direccionamiento al E.T.
+	MOVWF	SAVEFSR;Guardo el registro de direccionamiento indirecto
+;PCLATH
+	MOVF	PCLATH,W;El PCLATH no es retroalimentado por el PC 
+	MOVWF	SAVEPCL;por lo que se puede salvar ahora
+	CLRF	PCLATH;Y lo borramos por que queremos estar en el banco 0 y
+	; no queremos saltar a ningún banco de memoria
+
+	;AQUI TODAS LAS INTERRUPCIONES POR ORDEN DE PRIORIDAD.
+RETI:
+	CLRF	STATUS;
+	BTFSC	PIR1,TMR1IF;
+		GOTO	TIMER1INTER;
+	;AQUI ACABAN LAS INTERRUPCIONES
+
+;PCLATH
+	MOVF	SAVEPCL,W;
+	MOVWF	PCLATH;
+;FSR
+	MOVF	SAVEFSR,W;
+	MOVWF	FSR;
+;STATUS
+	MOVF	SAVEST,W;
+	MOVWF	STATUS;
+;W
+	MOVF	SAVEW,W;
+	RETFIE;
+	
+PROG:
+	BCF	INTCON,GIE;
+	CALL	INITTECLADO;
+	
+	
+	
+INITTECLADO:
+	BANKSEL	TRISPAD;Nos movemos al banco en el que esta la configuración del puerto del keypad
+	MOVLW	B'11110000';Configuramos como 4 entradas y 4 salidas
+	MOVWF	TRISPAD&7F;
+	BCF	OPTION_REG&7F,NOT_RBPU;Activamos las resistencias de pull up
+	BANKSEL PORTPAD;Nos ponemos en el banco del puerto del teclado
+	MOVLW	B'00000000';
+	MOVWF	PORTPAD;Ponemos a 0 todos los puertos para notar el cambio.
+	RETURN; 
