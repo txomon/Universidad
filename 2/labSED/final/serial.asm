6788b1e9526f6dc9df43a302887754b35c687b47
Javier Domingo <javierdo1@gmail.com>
2011-04-12 21:40:15 +0200

    empiezo a hacer la RSI del serial

--- 2/labSED/final/serial.asm
+++ 2/labSED/final/serial.asm
@@ -4,11 +4,20 @@
 
 ;****** SERIAL_INIT para inicializar la comunicación serial ******;
 SERIAL_INIT:
+	BANKSEL BAUDCTL;
+	MOVLW	B'00001000'; 16bit timer
+	MOVWF	BAUDCTL&7F;
 	BANKSEL	TXSTA;
-	MOVLW	B'00100100'
+	MOVLW	B'00100100';Habilitado, asincrono y alta velocidad
 	MOVWF	TXSTA&7f;
 	MOVLW	D'95'
 	MOVWF	SPBRG&7F;
 	CLRF	SPBRGH&7F;
-	
+	BANKSEL	RCSTA
+	MOVLW	B'10010000';Habilitado, Habilitado receptor
+	MOVWF	RCSTA
 	RETURN;
+	
+;****** SERIAL_SEND *******;
+SERIAL_SEND:
+	
