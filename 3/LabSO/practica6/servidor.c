c5deb002d39f3338155022b552ced95fd1c4c4fe
Javier Domingo <javierdo1@gmail.com>
2011-11-29 15:50:16 +0100

    Ya sale la parte del servidor muy bien, no esta multithread ni nada, pero voy a implementar el cliente por que no me deja mandar caracter a caracter

--- 3/LabSO/practica6/servidor.c
+++ 3/LabSO/practica6/servidor.c
@@ -5,19 +5,53 @@
 #include <netinet/in.h>
 
 int main (int args, char *argv[]){
-    int sockfd,x;
+    int sockfd,msgsock,x;
     struct sockaddr_in addr_mio,addr_cliente;
+    char data;
+
+
     sockfd=socket(AF_INET,SOCK_STREAM,0);
     if(0>sockfd){
         perror("Error creando el socket");
+        exit(1);
     }
+    printf("Socket creado\n");
+
     addr_mio.sin_family=AF_INET;
-    addr_mio.sin_port=5284;
+    addr_mio.sin_port=htons(5284);
     addr_mio.sin_addr.s_addr=INADDR_ANY;
     if(bind(sockfd,(struct sockaddr *) &addr_mio, sizeof(addr_mio))){
         perror("Inicializando el socket");
         exit(1);
     }
-    accept(sockfd,0,0);
+    printf("Inicializado el socket\n");
+
+    x=sizeof(addr_mio);
+    if(getsockname(sockfd,(struct sockaddr *) &addr_mio,&x)){
+        perror("getsockname()");
+    }
+    printf("El servidor se ha puesto en el puerto %d\n", 
+        ntohs(addr_mio.sin_port));
+
+    if(listen(sockfd,5)==-1){
+        perror("Escuchando el socket");
+        exit(1);
+    }
+    printf("Escuchando en el socket\n");
+
+    msgsock=accept(sockfd,0,0);
+    if(msgsock==-1)
+        perror("accept");
+    printf("Aceptada conexión\n");
+    /* Aqui viene la parte en la que hacemos algo */
+    
+    do{
+        read(msgsock,&data,1);
+        if(data!=-1)
+            printf("%c",data);
+            fflush(stdout);
+    }while(data!=-1);
+
+
     return 0;
 }
