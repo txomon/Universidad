456ff67cd3c372fe013d910de5bbb74bb7583d55
Javier Domingo <javierdo1@gmail.com>
2011-11-29 09:19:48 +0100

    Ya he cumplido con los requerimientos

--- 3/LabSO/practica5/mils.c
+++ 3/LabSO/practica5/mils.c
@@ -7,59 +7,88 @@
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <time.h>
 
 #define MI_RECUR   0x0001
 #define MI_LISTA   0X0002
 #define MI_TODOS   0X0004
 
-#define SMI_RECUR (X) (X & MI_RECUR)
-#define SMI_LISTA (X) (X & MI_LISTA)
-#define SMI_TODOS (X) (X & MI_TODOS)
+#define SMI_RECUR(X) (X & MI_RECUR)
+#define SMI_LISTA(X) (X & MI_LISTA)
+#define SMI_TODOS(X) (X & MI_TODOS)
 
 int mostrardir( char [],int);
 
 int mostrardir(char dir_origen[],int flags)
 {
-    char existe=1,*ndiror;
-    struct stat buf_or;
+    char *ndiror;
+    struct stat buf_or,element;
     struct dirent *dir_element;
-    DIR *dde,*dor;
+    DIR *dor;
+    struct tm *fecha;
 
     stat(dir_origen,&buf_or);
     printf("Empezamos a leer el directorio %s\n", dir_origen);
-    if(S_ISREG(buf_or->st_mode))
+    if(buf_or.st_mode&S_IFDIR){
         dor=opendir(dir_origen);
         while(NULL!=(dir_element=readdir(dor))){
+            ndiror=calloc(strlen(dir_element->d_name)
+                +strlen(dir_origen)+2,sizeof(char));
+            strcpy(ndiror,dir_origen);
+            if(ndiror[strlen(dir_origen)-1]!='/')
+                strcat(ndiror,"/");
+            strcat(ndiror,dir_element->d_name);
+            stat(ndiror,&element);
+            fecha=localtime(&(element.st_atime));       
             if((strcmp(dir_element->d_name,".")==0)||
                 (strcmp(dir_element->d_name,"..")==0)){
-                dir_element=readdir(dor);
-                if(NULL==dir_element)
-                    break;
+                if(SMI_TODOS(flags)){
+                    if(SMI_LISTA(flags))
+                        printf("%c\t%d\t%d\t%d/%d/%d\t", 
+                            (DT_DIR&dir_element->d_type)? 'D':
+                            (DT_REG&dir_element->d_type)? 'R':
+                            (DT_CHR&dir_element->d_type)? 'C':
+                            (DT_BLK&dir_element->d_type)? 'B':
+                            (DT_FIFO&dir_element->d_type)? 'F': '?',
+                            (int)element.st_nlink,(int)element.st_size,
+                            fecha->tm_mday,
+                            fecha->tm_mon,fecha->tm_year%100);
+                    printf("%s\n",dir_element->d_name);
+                }
+                goto bucle;
             }
-
-            printf("%s",dir_element->d_name);
-            ndiror=calloc(strlen(dir_element->d_name)+strlen(dir_origen)+2
-                ,sizeof(char));
-            strcpy(ndiror,dir_origen);
-            strcat(ndiror,"/");
-            strcat(ndiror,dir_element->d_name);
-            mostrardir(ndiror);
+            if(SMI_LISTA(flags))
+                printf("%c\t%d\t%d\t%d/%d/%d\t", 
+                    (DT_DIR&dir_element->d_type)? 'D':
+                    (DT_REG&dir_element->d_type)? 'R':
+                    (DT_CHR&dir_element->d_type)? 'C':
+                    (DT_BLK&dir_element->d_type)? 'B':
+                    (DT_FIFO&dir_element->d_type)? 'F': '?',
+                    (int)element.st_nlink,(int)element.st_size,
+                    fecha->tm_mday,fecha->tm_mon,fecha->tm_year%100);
+                    
+            printf("%s\n",dir_element->d_name);
+            if(SMI_RECUR(flags)){
+                mostrardir(ndiror,flags);
+            }
+            bucle:
             free(ndiror);
         }
+        free(dor);
+    }
     return 0;
 }
 
 int main(int args, char* argv[]){
     int flags=0;
     int x;
-    char opt,recursivo;
-    int *ficheros;
+    char opt;
 
-    printf("args=%d",args);
+    printf("args=%d\n",args);
     for(x=0;x<args;x++)
         printf("El arg[%d] => \"%s\"\n",x,argv[x]);
     /* Cojemos los argumentos y los interpretamos */
-    while(-1!=(opt=getopt(args,argv,"al"))){
+    while(-1!=(opt=getopt(args,argv,"alR"))){
         switch(opt){
         case 'a':
             flags|=MI_TODOS;
