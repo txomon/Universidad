4bf0517f2a0fb8f7a590743016193c94c842599f
Javier Domingo <javierdo1@gmail.com>
2012-02-28 12:45:35 +0100

    He seguido haciendo el 1.2 (aunque me este en la carpeta de práctica 2), y he hecho dos eventos, uno que siga periodicamente y otro que reporte al final, he dejado deshabilitado el periódico

--- 3/LabTD/practica2/main.tcl
+++ 3/LabTD/practica2/main.tcl
@@ -5,28 +5,61 @@ set simulacion [new Simulator]
 Tracefile set debug_ 0
 
 # Compruebo que haya argumentos, que si no, da error
-if { $argc != 1 } {
+if { $argc < 1 } {
     puts "Pon un argumento con la ruta al archivo que se quiere"
     exit 0
-} else {
+} elseif { $argc == 1 } {
+    set tam_cola 1000
     set fichero [lindex $argv 0]
     puts "Usando el fichero $fichero como origen de trazas"
+} else {
+    set tam_cola [lindex $argv 1]
+    set fichero [lindex $argv 0]
+    puts "Usando el fichero $fichero como origen de trazas"
+    puts "Tamaño de cola de $tam_cola"
 }
+
 # Definimos los colores para cada tipo de tráfico
 $simulacion color 1 Blue
 $simulacion color 2 Red
 
 # Abrimos un archivo como escritura y lo hacemos salida para los datos de 
 # representación nam
-set tr [open out.tr w]
+set traza_paquetes [open paquetes.tr w]
+set traza_monitor [open monitor.tr w]
+
+# Función que imprime el número total de paquetes tirados y de paquetes enviados
+proc sacar_info_final_monitor {} {
+    global monitor
+    puts "parrivals_\tpdrops_\tpdepartures_"
+    puts "[$monitor set parrivals_]\t\t[$monitor set pdrops_]\t[$monitor set pdepartures_]"
+}
 
 # Creamos la funcion de cierre, en la que se ejecuta el comando nam
 proc finish {} {
-        global simulacion tr
-        $simulacion flush-trace
-        close $tr
-        exit 0
+    sacar_info_final_monitor
+    global simulacion
+    global traza_paquetes
+    global traza_monitor
+    $simulacion flush-trace
+    close $traza_paquetes
+    close $traza_monitor
+    exit 0
 }
+
+set pdrop_ant 0
+# Función de sacar la información del monitor
+proc sacar_info_monitor {} {
+    global simulacion
+    global monitor
+    global pdrop_ant
+
+    set ahora [$simulacion now]
+    puts "$ahora [$monitor set pkts_] [expr [$monitor set pdrops_] - $pdrop_ant]"
+    set pdrop_ant [$monitor set pdrops_] 
+    $simulacion at [expr $ahora+0.01] "sacar_info_monitor"
+}
+
 
 # Creamos los nodos
 set fuente_1 [$simulacion node]
@@ -43,6 +76,9 @@ $simulacion simplex-link $acceso $destino 100Mb 10ms DropTail
 $simulacion simplex-link-op $fuente_1 $acceso orient right-down
 $simulacion simplex-link-op $fuente_2 $acceso orient right-up
 $simulacion simplex-link-op $acceso $destino orient right
+ 
+# Imponemos la limitación de tamaño de cola
+$simulacion queue-limit $acceso $destino $tam_cola
 
 # Creamos los agentes inyectores
 set inyector_1 [new Agent/UDP]
@@ -50,7 +86,11 @@ set inyector_2 [new Agent/UDP]
 set receptor [new Agent/Null]
 
 # Creamos el objeto de trazas
-$simulacion trace-queue $acceso $destino $tr
+$simulacion trace-queue $acceso $destino $traza_paquetes
+
+
+# Creamos el monitor del enlace
+set monitor [ $simulacion monitor-queue $acceso $destino $traza_monitor]
 
 # Definimos los tráficos como de diferentes clases, y les asociamos
 # diferentes colores
@@ -71,13 +111,12 @@ set ficherodetrazas [new Tracefile]
 
 # Configuramos el creador de tráfico CBR1 para que inyecte 
 # paquetes de 20 bytes a intervalos de 0.016 y lo añadimos al nodo
-$CBR1 set packetSize_ 2000
+$CBR1 set packetSize_ 20
 $CBR1 set interval_ 0.016
 $CBR1 attach-agent $inyector_1
 
 # Configuramos el contenedor de trazas
-$ficherodetrazas filename $fichero; #FIXME 
-# $ficherodetrazas filename  poisson_0.75_100Mbps_350.tr
+$ficherodetrazas filename $fichero
 
 # Configuramos el creador de tráfico personalizado para que inyecte 
 # paquetes del contenedor de trazas y lo añadimos al nodo
@@ -94,6 +133,9 @@ $simulacion at 9.99 "$CBR1 stop"
 
 $simulacion at 0.1 "$poisson1 start"
 $simulacion at 9.99 "$poisson1 stop"
+
+# Empezamos a sacar la información del monitor
+#$simulacion at 0.0 "sacar_info_monitor"
 
 # Creamos el evento de finalización
 $simulacion at 10 "finish"
